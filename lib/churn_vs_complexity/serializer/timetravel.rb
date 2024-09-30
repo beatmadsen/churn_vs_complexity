07b1b32306bbfd3c634fe75bdf4d956c8a64b190
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-10-01 01:23:00 +0200

    Timetravel: graph with example datapoints working

--- lib/churn_vs_complexity/serializer/timetravel.rb
+++ lib/churn_vs_complexity/serializer/timetravel.rb
@@ -3,14 +3,15 @@
 module ChurnVsComplexity
   module Serializer
     module Timetravel
+      def self.summaries(result)
+        result.sort_by do |sha, summary|
+          summary['end_date']
+        end.map { |entry| entry[1] }
+      end
+
       module CSV
         def self.serialize(result)
-          # result is a hash with sha keys and values are summary hashes
-
-          # 1. convert to array of summaries sorted by date ascending
-          summaries = result.sort_by do |sha, summary|
-            summary['end_date']
-          end.map { |entry| entry[1] }
+          summaries = Timetravel.summaries(result)
 
           # 2. Add title row to front of summaries
           summaries.unshift({ 
@@ -32,22 +33,23 @@ module ChurnVsComplexity
         end
       end
 
-      module Graph
+      class Graph
         def initialize(template: Graph.load_template_file)
           @template = template
         end
 
         def serialize(result)
+          summaries = Timetravel.summaries(result)
         #   data = result[:values_by_file].map do |file, values|
         #     "{ file_path: '#{file}', churn: #{values[0]}, complexity: #{values[1]} }"
         #   end.join(",\n") + "\n"
         #   title = Serializer.title(result)
-        #   @template.gsub("// INSERT DATA\n", data).gsub('INSERT TITLE', title)
-          'Graph baby'
+          title = "Some title"
+          @template.gsub('INSERT TITLE', title) # .gsub("// INSERT DATA\n", data)
         end
 
         def self.load_template_file
-          file_path = File.expand_path('../../tmp/template/timetravel_graph.html', __dir__)
+          file_path = File.expand_path('../../../tmp/template/timetravel_graph.html', __dir__)
           File.read(file_path)
         end
       end
