5d4638c44063907d477ea2a52de67df9347e8b6e
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-10-11 11:05:47 +0200

    a timetravel test case for graph as well

--- test/timetravel_integration_test.rb
+++ test/timetravel_integration_test.rb
@@ -15,4 +15,17 @@ class TimetravelIntegrationTest < TLDR
     refute_nil result
     refute_empty result
   end
+
+  def test_ruby_graph_completes
+    config = ChurnVsComplexity::Timetravel::Config.new(
+      language: :ruby,
+      serializer: :graph,
+      jump_days: 3_000,
+      relative_period: :quarter,
+    )
+    config.validate!
+    result = config.checker.check(folder: ChurnVsComplexity::ROOT_PATH)
+    refute_nil result
+    refute_empty result
+  end
 end
