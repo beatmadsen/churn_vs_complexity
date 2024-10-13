e925c83bd79bef6fb248220d9942a3e3cf50d282
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-10-14 00:34:58 +0200

    Integration testing multiple deltas and CSV

--- test/delta_integration_test.rb
+++ test/delta_integration_test.rb
@@ -6,6 +6,21 @@ class DeltaIntegrationTest < TLDR
   def test_ruby_csv_completes
     config = ChurnVsComplexity::Delta::Config.new(
       language: :ruby,
+      serializer: :csv,
+      commits: ['b20a9bddd04afb9c7a736cf4530fd3188b5f785e', '4f3151f83e982eb8f2b8a4e7a7572a0af156c3c0'],
+    )
+    config.validate!
+    result = config.checker.check(folder: 'lib')
+
+    csv_file_path = File.join(ChurnVsComplexity::ROOT_PATH, 'tmp/test-support/delta/ruby.csv')
+    expected = File.read(csv_file_path)
+
+    assert_equal expected, result
+  end
+
+  def test_ruby_summary_completes
+    config = ChurnVsComplexity::Delta::Config.new(
+      language: :ruby,
       serializer: :summary,
       commits: ['b20a9bddd04afb9c7a736cf4530fd3188b5f785e', '4f3151f83e982eb8f2b8a4e7a7572a0af156c3c0'],
     )
